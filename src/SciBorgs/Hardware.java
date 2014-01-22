b69e7a50ef84e26c9210a6ca404046823e43aa5a
meydany <meydany@007-07.bxsci.78x445.nycboe.org>
2014-01-22 16:21:17 -0500

    Added drive hardware to hardware class

--- src/SciBorgs/Hardware.java
+++ src/SciBorgs/Hardware.java
@@ -4,6 +4,10 @@
  */
 package SciBorgs;
 
+import edu.wpi.first.wpilibj.Gyro;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.Talon;
+
 /**
  * Contains all robot components including access to driver station.
  * Reference this class to access components
@@ -11,5 +15,18 @@ package SciBorgs;
  * @author Sciborgs
  */
 public class Hardware {
+    
+    //Jaguars for drive
+    Talon frontright = new Talon(1);
+    Talon frontleft = new Talon(2);
+    Talon backright = new Talon(3);
+    Talon backleft = new Talon(4);
+    
+    //Gyro for drive code
+    public Gyro direction = new Gyro(1);
+    
+    //Joysticks 
+    public Joystick rightjoy = new Joystick(1);
+    public Joystick leftjoy = new Joystick(2);
     
 }
