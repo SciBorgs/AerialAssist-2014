b5e0e0a8228f5f9a6b7c2efa5fefe4478843cc8a
Arun Bishop <arunleob@gmail.com>
2014-01-23 17:05:07 -0500

    Create Drive.java
    Simplified Drive without the gyro code.

---
+++ src/SciBorgs/Drive.java
@@ -0,0 +1,36 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package SciBorgs;
+
+import edu.wpi.first.wpilibj.Gyro;
+import edu.wpi.first.wpilibj.Jaguar;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.Timer;
+
+/**
+ * Teleop code.
+ * 
+ * @author Freshmen  
+ */
+public class DriveThread extends Thread{
+    
+    //joystick values for drive
+    double rightval;
+    double leftval;
+              
+    pubic void main() {
+                             
+        //Drive 
+        rightval = hardware.rightjoy.getY();
+        leftval = hardware.leftjoy.getY();
+
+        //Setting speed
+        hardware.frontright.set(-1 * rightval);
+        hardware.backright.set(-1 * rightval);
+
+        hardware.frontleft.set(leftval);
+        hardware.backleft.set(leftval);
+    }
+}
