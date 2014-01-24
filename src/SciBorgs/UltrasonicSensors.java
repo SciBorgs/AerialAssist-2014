dd203a2beebae2e6529f4e1d19c3334462208e7e
meydany <meydany@bxscience.edu>
2014-01-23 22:03:34 -0500

    Finished ultrasonic code

--- src/SciBorgs/UltrasonicSensors.java
+++ src/SciBorgs/UltrasonicSensors.java
@@ -14,15 +14,28 @@ import edu.wpi.first.wpilibj.Ultrasonic;
  */
 public class UltrasonicSensors {
     
+    Hardware hardware = new Hardware();
+    
     int ping = 1; //Going to change
     int echo = 2; //Going to change
-    
+    int button = 5; //Going to change
+       
     Ultrasonic sensor = new Ultrasonic(ping, echo);
     
+    public void turnOn() {
+        sensor.setEnabled(true);
+    }
+    
     public double getRange(){
-        if (sensor.isEnabled() == true && sensor.isRangeValid() == true){
+        
+        if(sensor.isEnabled() == true && sensor.isRangeValid() == true) {
             return sensor.getRangeInches();
         }
+        
         return 0;
+    }
+    
+    public void turnOff() {
+        sensor.setEnabled(false);
     }
 }
