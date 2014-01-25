96b670be8962316947ab1d02f448482c1ff2ba14
meydany <meydany@007-24.bxsci.78x445.nycboe.org>
2014-01-25 13:01:12 -0500

    Updated hardware from the previous repo

--- src/edu/wpi/first/wpilibj/templates/Hardware.java
+++ src/edu/wpi/first/wpilibj/templates/Hardware.java
@@ -9,6 +9,7 @@ import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.Talon;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.Compressor;
+import edu.wpi.first.wpilibj.RobotDrive;
 import edu.wpi.first.wpilibj.Ultrasonic;
 
 /**
@@ -20,22 +21,24 @@ import edu.wpi.first.wpilibj.Ultrasonic;
 public class Hardware {
     
     //Jaguars for drive
-    public static final Talon frontright = new Talon(1);
-    public static final Talon frontleft = new Talon(2);
-    public static final Talon backright = new Talon(3);
-    public static final Talon backleft = new Talon(4);
+    public static final Talon frontRightTalon = new Talon(1);
+    public static final Talon frontLeftTalon = new Talon(2);
+    public static final Talon backRightTalon = new Talon(3);
+    public static final Talon backLeftTalon = new Talon(4);
     
     //Gyro for drive code
     public static final Gyro gyro = new Gyro(1);
     
     //Joysticks 
-    public static final Joystick rightjoy = new Joystick(1);
-    public static final Joystick leftjoy = new Joystick(2);
+    public static final Joystick rightJoy = new Joystick(1);
+    public static final Joystick leftJoy = new Joystick(2);
     
     //Pistons
     public static final DoubleSolenoid piston = new DoubleSolenoid(1,2);
     public static final Compressor compress = new Compressor(1, 2);
     
     //Ultrasonic sensor
-    public static final Ultrasonic sensor = new Ultrasonic(1, 2);
+    public static final Ultrasonic leftSensor = new Ultrasonic(1, 2);
+    public static final Ultrasonic rightSensor = new Ultrasonic(3, 4);
+
 }
