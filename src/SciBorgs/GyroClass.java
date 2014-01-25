ead1dffb1e83ee7fab118e1af92d0754a87aea34
abot98 <arunleob@gmail.com>
2014-01-24 22:47:28 -0500

    Reverted Package

--- src/SciBorgs/GyroClass.java
+++ src/SciBorgs/GyroClass.java
@@ -5,15 +5,16 @@ package SciBorgs;
  * @author meydany, luzono
  */
 public class GyroClass{
-
+    
+    Hardware hardware = new Hardware();
     double angle;
     
     public void startup(){
-        Hardware.gyro.reset();
+        hardware.direction.reset();
     }
     
     protected void iteration(){
-        angle = Hardware.gyro.getAngle();
+        angle = hardware.direction.getAngle();
         
         while (angle>=360){
             angle -= 360;
