2f12ed746fee6183ffd17ee2e12197239e6156db
Arun Bishop <arunleob@gmail.com>
2014-02-04 16:13:58 -0500

    Updated Drive.java
    Reverted to using exponential drive, because robotDrive is not currently operating correctly.  If changed, please comment the code out, do NOT delete it.

--- src/edu/wpi/first/wpilibj/templates/Drive.java
+++ src/edu/wpi/first/wpilibj/templates/Drive.java
@@ -9,12 +9,26 @@ import edu.wpi.first.wpilibj.Joystick;
 
 public class Drive extends ScibotThread {
     
-  
-    public void start() {
-        Hardware.drive.tankDrive(Hardware.leftJoy, Hardware.rightJoy);
-    }
+    double rightVal, leftVal;
     
     public void main() {   
+        //Drive
+        rightVal = ScibotManager.hardware.rightJoy.getY();
+        leftVal = ScibotManager.hardware.leftJoy.getY();
+        
+        if(rightVal > 1) rightVal = 1;
+        if(rightVal < -1) rightVal = -1;
+        if(leftVal > 1) leftVal = 1;
+        if(leftVal < -1) leftVal = -1;
+
+        rightVal *= Math.abs(rightVal);
+        leftVal *= Math.abs(leftVal);
+
+        ScibotManager.hardware.frontRightTalon.set(rightVal);
+        ScibotManager.hardware.backRightTalon.set(rightVal);
+        ScibotManager.hardware.frontLeftTalon.set(-leftVal);
+        ScibotManager.hardware.backLeftTalon.set(-leftVal);
+        
         Hardware.dLCD.println(DriverStationLCD.Line.kUser6, 1, String.valueOf(Hardware.leftJoy.getAxis(Joystick.AxisType.kY)) + " " + String.valueOf(Hardware.rightJoy.getAxis(Joystick.AxisType.kY)));
         Hardware.dLCD.updateLCD();
 
