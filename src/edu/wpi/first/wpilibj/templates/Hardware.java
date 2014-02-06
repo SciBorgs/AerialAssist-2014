e40957aad6fde9cd45ff3e2804d4d7a2d04614ed
Brian Hong <hongs2@bxscience.edu>
2014-02-06 17:21:17 -0500

    Revert "Update Hardware.java"
    This reverts commit f81ef7fee3a5c283723892d632ff78df4973c2a3.

--- src/edu/wpi/first/wpilibj/templates/Hardware.java
+++ src/edu/wpi/first/wpilibj/templates/Hardware.java
@@ -25,55 +25,55 @@ import edu.wpi.first.wpilibj.camera.AxisCamera;
 public class Hardware {
     
     //Talons for drive
-    public static Talon frontRightTalon = new Talon(1);
-    public static Talon frontLeftTalon = new Talon(3);
-    public static Talon backRightTalon = new Talon(2);
-    public static Talon backLeftTalon = new Talon(4);
+//    public static Talon frontRightTalon;
+//    public static Talon frontLeftTalon;
+//    public static Talon backRightTalon;
+//    public static Talon backLeftTalon;
     
     //Robotdrive (1=fl,2=bl,3=fr,4=br)
-    // public static RobotDrive drive;
+    public static RobotDrive drive;
     
     //Gyro for drive code
     //public static Gyro gyro;
     
     //Joysticks 
-    public static Joystick rightJoy = new Joystick(1);
-    public static Joystick leftJoy = new Joystick(2);
+    public static Joystick rightJoy;
+    public static Joystick leftJoy;
     
-    public static DriverStationLCD dLCD = DriverStationLCD.getInstance();
+    public static DriverStationLCD dLCD;
     
     //Pistons
-    public static DoubleSolenoid shooterPiston1 = new DoubleSolenoid(1,2);
-    public static DoubleSolenoid shooterPiston2 = new DoubleSolenoid(3,4);
-    public static Solenoid gateLatch = new Solenoid(3);
+    public static DoubleSolenoid shooterPiston1;
+    public static DoubleSolenoid shooterPiston2;
+    public static Solenoid gateLatch;
     //public static DoubleSolenoid piston = new DoubleSolenoid(1,2);
-    public static Compressor compressor = new Compressor(1,2);
+    public static Compressor compressor;
     
     //Camera
-    public static AxisCamera camera = AxisCamera.getInstance();
+    public static AxisCamera camera;
     
     //Ultrasonic
-    public static Ultrasonic leftSensor = new Ultrasonic(1,2);
-    public static Ultrasonic rightSensor = new Ultrasonic(3,4);
+    public static Ultrasonic leftSensor;
+    public static Ultrasonic rightSensor;
    
     //make sense to let the Hardware class handle instantiation
-    // public Hardware(){
-    //     frontRightTalon = new Talon(3);
-    //     backRightTalon = new Talon(4);
-    //     frontLeftTalon = new Talon(1);
-    //     backLeftTalon = new Talon(2);
+    public Hardware(){
+//        frontRightTalon = new Talon(3);
+//        backRightTalon = new Talon(4);
+//        frontLeftTalon = new Talon(1);
+//        backLeftTalon = new Talon(2);
         
-    //     drive = new RobotDrive(1,2,3,4);//(1=fl,2=bl,3=fr,4=br)
-    //     //gyro = new Gyro(1);
-    //     dLCD = DriverStationLCD.getInstance();
-    //     camera = AxisCamera.getInstance();
-    //     rightJoy = new Joystick(1);
-    //     leftJoy = new Joystick(2);
-    //     shooterPiston1 = new DoubleSolenoid(1,2);
-    //     shooterPiston2 = new DoubleSolenoid(3,4);
-    //     gateLatch = new Solenoid(3);
-    //     compressor = new Compressor(1,2);
-    //     leftSensor = new Ultrasonic(1,2);
-    //     rightSensor = new Ultrasonic(3,4);
-    // }
+        drive = new RobotDrive(1,2,3,4);//(1=fl,2=bl,3=fr,4=br)
+        //gyro = new Gyro(1);
+        dLCD = DriverStationLCD.getInstance();
+        camera = AxisCamera.getInstance();
+        rightJoy = new Joystick(1);
+        leftJoy = new Joystick(2);
+        shooterPiston1 = new DoubleSolenoid(1,2);
+        shooterPiston2 = new DoubleSolenoid(3,4);
+        gateLatch = new Solenoid(3);
+        compressor = new Compressor(1,2);
+        leftSensor = new Ultrasonic(1,2);
+        rightSensor = new Ultrasonic(3,4);
+    }
 }
