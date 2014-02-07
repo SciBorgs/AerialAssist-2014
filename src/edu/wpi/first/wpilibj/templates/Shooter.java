035d4b44dce3607d3486cf368d71605707cc39cc
Arun Bishop <bishopa1@bxscience.edu>
2014-02-07 17:49:06 -0500

    Updated code 2/7/2014

--- src/edu/wpi/first/wpilibj/templates/Shooter.java
+++ src/edu/wpi/first/wpilibj/templates/Shooter.java
@@ -1,14 +1,20 @@
-public class Shooter extends SciBotThread{
+package edu.wpi.first.wpilibj.templates;
+
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.Relay;
+import edu.wpi.first.wpilibj.Timer;
+
+public class Shooter extends ScibotThread{
   
-  public final int SHOOT_TIME = 100; //Time, in seconds, it takes to shoot
+  public static final int SHOOT_TIME = 100; //Time, in seconds, it takes to shoot
   
   public void function(){
-    //Code to turn compressor on and off based on value of pressure sensor
-    if(!Hardware.compress.getPressureSwitchValue()) {
-      Hardware.compress.start();
+    //Code to turn compressoror on and off based on value of pressure sensor
+    if(!Hardware.compressor.getPressureSwitchValue()) {
+      Hardware.compressor.start();
     }
     else {
-      compress.stop();
+      Hardware.compressor.stop();
     }
       
     if (Hardware.rightJoy.getTrigger()){
@@ -18,20 +24,20 @@ public class Shooter extends SciBotThread{
   
   public static void shoot(){
     //Code to shoot
-      Hardware.relay.set(Relay.kForward);
-      Hardware.relay.set(Relay.kOn);
-      Hardware.piston1.set(DoubleSolenoid.kForward);
-      Hardware.piston2.set(DoubleSolenoid.kForward);
+      Hardware.relay.set(Relay.Value.kForward);
+      Hardware.relay.set(Relay.Value.kOn);
+      Hardware.piston1.set(DoubleSolenoid.Value.kForward);
+      Hardware.piston2.set(DoubleSolenoid.Value.kForward);
       
       Timer.delay(SHOOT_TIME);
       
-      Hardware.relay.stop();
-      Hardware.piston1.set(DoubleSolenoid.kReverse);
-      Hardware.piston2.set(DoubleSolenoid.kReverse);
+      Hardware.relay.set(Relay.Value.kOff);
+      Hardware.piston1.set(DoubleSolenoid.Value.kReverse);
+      Hardware.piston2.set(DoubleSolenoid.Value.kReverse);
       
       Timer.delay(SHOOT_TIME);
       
-      Hardware.piston1.set(DoubleSolenoid.kOff);
-      Hardware.piston2.set(DoubleSolenoid.kOff);
+      Hardware.piston1.set(DoubleSolenoid.Value.kOff);
+      Hardware.piston2.set(DoubleSolenoid.Value.kOff);
   }
 }
