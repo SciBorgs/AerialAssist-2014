d3439a1d8502e97429295421802d67f77e755aab
Yoli Meydan <meydany@bxscience.edu>
2014-05-28 16:41:30 -0500

    Update AutoUltrasonic.java
    Commented some stuff out...not entirely finished

--- src/edu/wpi/first/wpilibj/templates/AutoUltrasonic.java
+++ src/edu/wpi/first/wpilibj/templates/AutoUltrasonic.java
@@ -4,9 +4,9 @@
 //
 //public class AutoUltrasonic extends ScibotThread {
 //
-//    double leftValue = Hardware.leftSensor.getRangeInches();
-//    double rightValue = Hardware.rightSensor.getRangeInches();
-//    double avgValue = (leftValue + rightValue) / 2;
+//    double leftValue = Hardware.leftSensor.getRangeInches(); //sets the range (in inches) that is read by the left sensor to leftValue
+//    double rightValue = Hardware.rightSensor.getRangeInches(); //sets the range (in inches) that is read by the right sensor to rightValue
+//    double avgValue = (leftValue + rightValue) / 2; //Average of the two values (we just need the general distance from straight ahead)
 //    final double BUFFER = 1;
 //    double distance = 1;
 //    boolean ready = false;
@@ -20,17 +20,17 @@
 //    //Function that aligns and gets the robot to the proper distance, then 
 //    //
 //    public void function() {
-//        if(!Vision.targetHot.booleanValue()) return;
+//        if(!Vision.targetHot.booleanValue()) return; //Checks if target is hot. If it isn't, it returns and does nothing.
 //        else {
-//            Shooter.shoot();
-//            Shooter.withdraw();
+//            Shooter.shoot(); //if target is hot, shoot
+//            Shooter.withdraw(); //after shooting is finished, withdraw claw
 //        }
 //        if(!ready) {
-//            while(Math.abs(leftValue - rightValue) > BUFFER) {
-//                align();
+//            while(Math.abs(leftValue - rightValue) > BUFFER) { //checks to see if leftValue and rightValue are not equal, and then realigns robot
+//                align(); //realigns robot until leftValue and rightValue are equal
 //            }
 //            while(!ready) {
-//                ready = travel();
+//                ready = travel(); 
 //            }
 //        }
 //        else {
@@ -49,17 +49,19 @@
 //    
 //    public void align() {
 //        double speed = (leftValue - rightValue) * 0.1;
-//        if (leftValue - rightValue > 0) {
+//        if (leftValue - rightValue > 0) { //this would be true if the robot is slanted so that the left side is closer to said object
+//            //realigns robot so that robot is straight
 //            Hardware.backLeftTalon.set(speed);
 //            Hardware.frontLeftTalon.set(speed);
 //            Hardware.frontRightTalon.set(-speed);
 //            Hardware.backRightTalon.set(-speed);
-//        } else if (rightValue - leftValue > 0) {
+//        } else if (rightValue - leftValue > 0) { //this would be true if the robot is slanted so that the right side is closer to said object
+//            //realigns robot so that robot is straight
 //            Hardware.backLeftTalon.set(-speed);
 //            Hardware.frontLeftTalon.set(-speed);
 //            Hardware.frontRightTalon.set(speed);
 //            Hardware.backRightTalon.set(speed);
-//        } else {
+//        } else { //this would be true if robot is already straight and therefore robot would not need realignment 
 //            Hardware.backLeftTalon.set(0);
 //            Hardware.frontLeftTalon.set(0);
 //            Hardware.frontRightTalon.set(0);
@@ -93,6 +95,7 @@
 //        }
 //    }
 //    
+//    //returns the average range of the robot from the nearest object read by the sensor 
 //    public static double getRange() {
 //        if (Hardware.leftSensor.isEnabled() && Hardware.rightSensor.isEnabled() && Hardware.leftSensor.isRangeValid() && Hardware.rightSensor.isRangeValid()) {
 //            double average = (Hardware.leftSensor.getRangeInches() + Hardware.rightSensor.getRangeInches()) / 2;
