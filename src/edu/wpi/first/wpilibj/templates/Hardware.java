c3c9eefe6b8f83f8fccf9a46820aeba43b7aa21d
Lucas Cecchi <marcz@bxscience.edu>
2014-05-28 19:14:40 -0400

    Update Hardware.java
    Extra and more detailed comments

--- src/edu/wpi/first/wpilibj/templates/Hardware.java
+++ src/edu/wpi/first/wpilibj/templates/Hardware.java
@@ -3,7 +3,7 @@
  * and open the template in the editor.
  */
 package edu.wpi.first.wpilibj.templates;
-
+//All hardware imports
 import edu.wpi.first.wpilibj.AnalogChannel;
 import edu.wpi.first.wpilibj.Gyro;
 import edu.wpi.first.wpilibj.Joystick;
@@ -26,16 +26,16 @@ import edu.wpi.first.wpilibj.RobotDrive;
  */
 public class Hardware {
     
-    //Talons for drive
+    //Declares Talons for drive code
     public static Talon frontRightTalon;
     public static Talon frontLeftTalon;
     public static Talon backRightTalon;
     public static Talon backLeftTalon;
     
-    //Gyro for drive code
+    //Declares Gyro for gyrodrive code
 //    public static Gyro gyro;
     
-    //Joysticks 
+    //Declares Joysticks 
     public static Joystick rightJoy;
     public static Joystick leftJoy;
     public static Joystick remote;
@@ -44,29 +44,29 @@ public class Hardware {
     
 //    public static Ultrasonic leftSensor;
 //    public static Ultrasonic rightSensor;
-    
+    //Declares compressor
     public static Compressor compressor;
 //    public static Relay tempRelay;
-    
+    //Declares solenoids
     public static DoubleSolenoid piston;
     public static DoubleSolenoid gateLatch;
     //public static Compressor compress = new Compressor(1, 2);
     //Camera
-    
+    //Declares drive as an object of class RobotDrive
     public static RobotDrive drive;
 //    public static Relay topMotor;
     
     public Hardware(){
         //initializing all the harware
         //and assigning pins/ports
-        rightJoy = new Joystick(2);
-        leftJoy = new Joystick(1);
-        remote = new Joystick(3);
+        rightJoy = new Joystick(2); //sets right joystick to port 2
+        leftJoy = new Joystick(1); //sets left joystick to port 1
+        remote = new Joystick(3); //sets remote to port 3
         
-        frontRightTalon = new Talon(9);
-        backRightTalon = new Talon(10);
-        frontLeftTalon = new Talon(1);
-        backLeftTalon = new Talon(2);
+        frontRightTalon = new Talon(9);//sets front right talon channel to 9
+        backRightTalon = new Talon(10);//sets back right talon channel to 10
+        frontLeftTalon = new Talon(1);//sets front left talon channel to 1
+        backLeftTalon = new Talon(2);//sets back left talon channel to 2
 //        drive = new RobotDrive(1,2,9,10);
 //        tempRelay = new Relay(1);
         
@@ -74,12 +74,12 @@ public class Hardware {
         
 //        Hardware.leftSensor = new Ultrasonic(1, 1); //FIX PORT
 //        Hardware.rightSensor = new Ultrasonic(1, 1); //FIX PORT
-        compressor = new Compressor(10, 1); //updated port
+        compressor = new Compressor(10, 1); //updated port- sets channels for pressureswitch to 10 and relay to 1
         
-        gateLatch = new DoubleSolenoid(1,2);
-        gateLatch.set(DoubleSolenoid.Value.kOff);
-        piston = new DoubleSolenoid(3,4); //updated port
-        piston.set(DoubleSolenoid.Value.kOff);
+        gateLatch = new DoubleSolenoid(1,2);//sets cforward channel to 1 and reverse channel to 2
+        gateLatch.set(DoubleSolenoid.Value.kOff);//turns gatelatch completely off 
+        piston = new DoubleSolenoid(3,4); //updated port-sets forward channel to 3 and reverse channel to 4
+        piston.set(DoubleSolenoid.Value.kOff);//turns piston completely off
         //Hardware.claw = new DoubleSolenoid(5, 6); //updated port
 //        limit = new AnalogChannel(1);
         
